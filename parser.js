6041739d2683265711c29ffb4c20b573dcc6aed1
Rohit511 <rathorerohit511@gmail.com>
2023-03-30 09:57:25 +0530

    api

--- parser.js
+++ parser.js
@@ -9,18 +9,3 @@ const result = parser.xml2json(xml, options)
 const parsedResult = JSON.stringify(result, '');
 
 console.log(parsedResult);
-class API {
-    #secure;
-    constructor(url, method = 'GET') {
-      this.url = url;
-      this.method = method;
-      this.#secure = url.startsWith('https');
-    }
-    isSecure() {
-      return this.#secure;
-    }
-    updateUrl(url) {
-      this.url = url;
-      this.#secure = url.startsWith('https');
-    }
-  }
